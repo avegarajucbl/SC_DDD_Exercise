9dfcd10545412d7e8dabfded631c0de8bb2ba6ff
nrjohnstone <n.johnstone@coolblue.nl>
2017-04-14 17:06:15 +0200

    Flesh out scenarios and tests

---
+++ Domain/Class1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Cart
+    {
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void SetQuantity(Product product, uint quantity)
+        {
+        }
+
+        public Order Checkout(Customer customer, Payment payment)
+        {
+            if (payment.Amount < DetermineTotalPrice())
+            {
+                throw new PaymentSizeMattersException();
+            }
+
+            return new Order(payment);
+        }
+
+        private decimal DetermineTotalPrice()
+        {
+            return _products.Sum(p => p.Price);
+        }
+
+        private readonly List<Product> _products = new List<Product>();
+    }
+
+    public class Customer
+    {
+    }
+
+    public class Order
+    {
+        public Order(Payment payment)
+        {
+            Payment = payment;
+        }
+
+        public Payment Payment { get; private set; }
+
+        public void DenyPayment()
+        {
+            Payment.Reject();
+        }
+
+        public void VerifyPayment()
+        {
+            Payment.Verify();
+        }
+    }
+
+    public class Payment
+    {
+        public Payment(decimal amount)
+        {
+            this.Amount = amount;
+        }
+
+        public decimal Amount { get; }
+        public PaymentState State { get; private set; }
+
+        public void Reject()
+        {
+            State = PaymentState.Rejected;
+        }
+
+        public void Verify()
+        {
+            State = PaymentState.Verified;
+        }
+    }
+
+    public class Product
+    {
+        public decimal Price { get; }
+
+        public Product(decimal price)
+        {
+            this.Price = price;
+        }
+    }
+
+    public enum PaymentState
+    {
+        NotVerified,
+        Verified,
+        Rejected
+    }
+
+    public class PaymentSizeMattersException : Exception
+    {
+    }
+}
---
+++ Tests.Unit/CheckoutTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Domain;
+using FluentAssertions;
+using Xunit;
+
+namespace Tests.Unit
+{
+    public class CheckoutTests
+    {
+        /// <summary>
+        /// Given a cart has products
+        /// And the payment amount equals the total sales price of the products
+        /// When the customer checks out
+        /// Then an order is created
+        /// And payment is pending verification
+        /// </summary>
+        [Fact]
+        public void Checkout()
+        {
+            var cart = new Cart();
+
+            var product1 = new Product(price: 8.5m);
+            var product2 = new Product(price: 15.99m);
+            
+            cart.Add(product1);
+            cart.Add(product2);
+
+            var customer = new Customer();
+            var payment = new Payment(amount: product1.Price + product2.Price);
+            var order = cart.Checkout(customer, payment);
+
+            order.Payment.State.Should().Be(PaymentState.NotVerified);
+        }
+
+        /// <summary>
+        /// Given a cart has products
+        /// And the payment amount does not equal the total sales price of the products
+        /// When the customer checks out
+        /// Then an exception is thrown in its face
+        /// And there is much gnashing of teeth
+        /// </summary>
+        [Fact]
+        public void CheckoutWithDisappointingAmount()
+        {
+            var cart = new Cart();
+
+            var product1 = new Product(price: 8.5m);
+            var product2 = new Product(price: 15.99m);
+
+            cart.Add(product1);
+            cart.Add(product2);
+
+            var customer = new Customer();
+            var payment = new Payment(amount: product1.Price + product2.Price - 1m);
+
+            cart.Invoking(obj => obj.Checkout(customer, payment))
+                .ShouldThrow<PaymentSizeMattersException>();
+        }
+
+        /// <summary>
+        /// Given an order exists
+        /// And payment is pending
+        /// When the payment provider denies the payment
+        /// Then the payment is rejected
+        /// </summary>
+        [Fact]
+        public void PaymentDenied()
+        {
+            var order = new Order(new Payment(amount:20m));
+
+            order.DenyPayment();
+
+            order.Payment.State.Should().Be(PaymentState.Rejected);
+        }
+
+        /// <summary>
+        /// Given an order exists
+        /// And payment is pending
+        /// When the payment provider confirms the payment
+        /// Then the payment is verified
+        /// </summary>
+        [Fact]
+        public void PaymentConfirmed()
+        {
+            var order = new Order(new Payment(amount: 20m));
+
+            order.VerifyPayment();
+
+            order.Payment.State.Should().Be(PaymentState.Verified);
+        }
+    }
+}
